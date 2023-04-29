e23d3e46ead656aa7060849595467ae21e6957b0
Leonardo Rivera <LeonardoRivera678976@gmail.com>
2023-04-29 08:55:51 -0600

    Add: delete inner word backwards key binding

--- lua/androidb/remap.lua
+++ lua/androidb/remap.lua
@@ -4,3 +4,18 @@ vim.keymap.set("n","<leader>s",vim.cmd.w)
 --send target onto oblivion
 vim.keymap.set("x","<leader>p","\"_dP")
 vim.keymap.set("n","<leader>d","\"_dd")
+--change ctrl + backspace behaviour on insert mode
+vim.keymap.set("n","<C-b>", "\"_bdiw")
+
+
+
+
+
+
+
+
+    
+  
+ 
+   
+   
