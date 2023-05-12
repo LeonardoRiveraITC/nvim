9160729937061d9e83fe359ec3a5c3f5ea8c8b6a
LeonardoRiveraITC <19030342@itcelaya.edu.mx>
2023-05-11 21:24:02 -0600

    More remaps

--- lua/androidb/remap.lua
+++ lua/androidb/remap.lua
@@ -8,18 +8,8 @@ vim.keymap.set("n","dd","\"_dd")
 vim.keymap.set("n","<C-b>", "\"_bdiw")
 vim.keymap.set("n","diw", "\"_diw")
 vim.keymap.set("n","dw", "\"_dw")
-
-
-
-
-
-
-
-
-
-
-    
-  
- 
-   
-   
+vim.keymap.set("n","d$", "\"_d$")
+vim.keymap.set("n","d0", "\"_d0")
+vim.keymap.set("n","df", "\"_df")
+vim.keymap.set("n","di\"", "\"_di\"")
+vim.keymap.set("n","di'", "\"_di'")
